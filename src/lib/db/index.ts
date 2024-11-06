8b5bd6d4004f2b2aedfd5c243f13eca8e1742eb7
oYwyh <yassienwyh0@gmail.com>
2024-11-06 20:47:57 +0200

    stuff

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,20 +1,10 @@
-// local postgress
-// import { drizzle } from 'drizzle-orm/postgres-js' // the way we connect
-// import * as schema from './schema' // getting the schema
-// import postgres from 'postgres' // getting the database driver
-
-// import { type PostgresJsDatabase } from "drizzle-orm/postgres-js";
-
-// export const client = postgres(process.env.DATABASE_URL as string, { max: 1 })
-// export const db: PostgresJsDatabase<typeof schema> = drizzle(client, { schema, logger: true /* to log all the sql query we make */ })
-
-// export default db;
-
-// Vercel
-import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { sql } from "@vercel/postgres";
+import { drizzle } from 'drizzle-orm/postgres-js' // the way we connect
 import * as schema from './schema' // getting the schema
+import postgres from 'postgres' // getting the database driver
 
-export const db = drizzle({ client: sql, schema, logger: true })
+import { type PostgresJsDatabase } from "drizzle-orm/postgres-js";
+
+export const client = postgres(process.env.DATABASE_URL as string, { max: 1 })
+export const db: PostgresJsDatabase<typeof schema> = drizzle(client, { schema, logger: true /* to log all the sql query we make */ })
 
 export default db;
