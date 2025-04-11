1b099115b0e8ce5d5f4b472ada06fe3482eac159
ywyh <yassienwyh0@gmail.com>
2025-04-11 15:20:07 +0200

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,30 +15,30 @@ export async function middleware(request: NextRequest) {
       return NextResponse.redirect(new URL('/', request.url))
     }
 
-    // For admin-only routes
-    if (request.nextUrl.pathname.startsWith('/dashboard/admins') ||
-        request.nextUrl.pathname.startsWith('/dashboard/doctors') ||
-        request.nextUrl.pathname.startsWith('/dashboard/users') ||
-        request.nextUrl.pathname.startsWith('/dashboard/receptionists') ||
-        request.nextUrl.pathname.startsWith('/dashboard/services') ||
-        request.nextUrl.pathname.startsWith('/dashboard/faqs') ||
-        request.nextUrl.pathname.startsWith('/dashboard/settings')) {
-      if (data?.user?.role !== 'admin') {
-        return NextResponse.redirect(new URL('/', request.url))
-      }
-    }
+    // // For admin-only routes
+    // if (request.nextUrl.pathname.startsWith('/dashboard/admins') ||
+    //     request.nextUrl.pathname.startsWith('/dashboard/doctors') ||
+    //     request.nextUrl.pathname.startsWith('/dashboard/users') ||
+    //     request.nextUrl.pathname.startsWith('/dashboard/receptionists') ||
+    //     request.nextUrl.pathname.startsWith('/dashboard/services') ||
+    //     request.nextUrl.pathname.startsWith('/dashboard/faqs') ||
+    //     request.nextUrl.pathname.startsWith('/dashboard/settings')) {
+    //   if (data?.user?.role != 'admin') {
+    //     return NextResponse.redirect(new URL('/', request.url))
+    //   }
+    // }
 
-    // For admin or doctor routes
-    if (request.nextUrl.pathname.startsWith('/dashboard/presets')) {
-      if (data?.user?.role !== 'admin' && data?.user?.role !== 'doctor') {
-        return NextResponse.redirect(new URL('/', request.url))
-      }
-    }
+    // // For admin or doctor routes
+    // if (request.nextUrl.pathname.startsWith('/dashboard/presets')) {
+    //   if (data?.user?.role != 'admin' && data?.user?.role !== 'doctor') {
+    //     return NextResponse.redirect(new URL('/', request.url))
+    //   }
+    // }
 
-    // General dashboard access (any other dashboard routes)
-    if ((!user || user.role === 'user') && request.nextUrl.pathname.startsWith('/dashboard')) {
-      return NextResponse.redirect(new URL('/', request.url))
-    }
+    // // General dashboard access (any other dashboard routes)
+    // if ((!user || user.role == 'user') && request.nextUrl.pathname.startsWith('/dashboard')) {
+    //   return NextResponse.redirect(new URL('/', request.url))
+    // }
 
   } catch (error) {
     console.error('Auth middleware error:', error)
